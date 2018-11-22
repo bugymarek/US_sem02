537c6d80ae6085274897a99f4e9af4f9cb446808
bugymarek <bugymarek@gmail.com>
2018-11-22 23:02:29 +0100

    implementation of add records to dynamicHashing almost done.

--- src/dynamicHashingCore/DynamicHashing.java
+++ src/dynamicHashingCore/DynamicHashing.java
@@ -83,42 +83,71 @@ public class DynamicHashing<T> {
         Record newRecord = new Record(true, record);
         BitSet hash = Converter.getHashFromKey(record.getHashKey(), maxHashSize);
         Node currentNode = root;
+        int depth = 0;
         while (currentNode instanceof InternalNode) {
-            
+            if (hash.get(depth)) {
+                currentNode = ((InternalNode) currentNode).getRightNode();
+            } else {
+                currentNode = ((InternalNode) currentNode).getLeftNode();
+            }
+            depth++;
         }
 
-        if (((ExternalNode) currentNode).getAddressBlock() == -1) { //ak nieje alokovaný blok
-            Block block = new Block(getNextFreeAddress(), factor, templateRecord.getData());
-            boolean resultMemory = block.addRecord(newRecord);
-            boolean resultFile = writeToFile(block.getAddress() * TEMPLATE_BLOCK.getSize(), block.toByteArray(), mainFile);
-            if (resultMemory && resultFile) {
-                ((ExternalNode) currentNode).setAddressBlock(block.getAddress());
-                ((ExternalNode) currentNode).incrementValidRecordsCount();
-                return true;
-            } else {
-                System.out.println("nepodarilo sa vlozit záznam: " + record);
-                return false;
-            }
-        } else if (((ExternalNode) currentNode).getValidRecordsCount() < factor) { // ak je v bloku miesto miesto
-            Block block = TEMPLATE_BLOCK.fromByteArray(readFromFile(((ExternalNode) currentNode).getAddressBlock() * TEMPLATE_BLOCK.getSize(), mainFile));
-            if (block.findRecord(newRecord) != null) {
-                System.out.println("uz sa tam nachadza záznam s vkladaným ID: " + record.getHashKey());
-                return false;// uz sa tam nachadza záznam s vkladaným ID
-            } else {
+        while (true) {// osteri ak ma externz prvok max hlbku a v nom uz nieje miesto tak vznik kolizie... tato podmienka to zatial neosetruje
+            if (((ExternalNode) currentNode).getAddressBlock() == -1) { //ak nieje alokovaný blok
+                Block block = new Block(getNextFreeAddress(), factor, templateRecord.getData());
                 boolean resultMemory = block.addRecord(newRecord);
                 boolean resultFile = writeToFile(block.getAddress() * TEMPLATE_BLOCK.getSize(), block.toByteArray(), mainFile);
                 if (resultMemory && resultFile) {
+                    ((ExternalNode) currentNode).setAddressBlock(block.getAddress());
                     ((ExternalNode) currentNode).incrementValidRecordsCount();
-                    System.out.println(block.toString());
                     return true;
                 } else {
                     System.out.println("nepodarilo sa vlozit záznam: " + record);
                     return false;
                 }
+            } else if (((ExternalNode) currentNode).getValidRecordsCount() < factor) { // ak je v bloku miesto miesto
+                Block block = TEMPLATE_BLOCK.fromByteArray(readFromFile(((ExternalNode) currentNode).getAddressBlock() * TEMPLATE_BLOCK.getSize(), mainFile));
+                if (block.findRecord(newRecord) != null) {
+                    System.out.println("uz sa tam nachadza záznam s vkladaným ID: " + record.getHashKey());
+                    return false;// uz sa tam nachadza záznam s vkladaným ID
+                } else {
+                    boolean resultMemory = block.addRecord(newRecord);
+                    boolean resultFile = writeToFile(block.getAddress() * TEMPLATE_BLOCK.getSize(), block.toByteArray(), mainFile);
+                    if (resultMemory && resultFile) {
+                        ((ExternalNode) currentNode).incrementValidRecordsCount();
+                        System.out.println(block.toString());
+                        return true;
+                    } else {
+                        System.out.println("nepodarilo sa vlozit záznam: " + record);
+                        return false;
+                    }
+                }
+            } else { // ak nieje v bloku miesto
+                if(currentNode.getDepth() == maxHashSize)break; // kolizia. som v najväčšej hlbke. Preto nemôžem už vytvárať dalšie externe vrcholy a musím riešiť kolíziu
+                Node newInternalNode = new InternalNode(currentNode.getFather(), currentNode.getDepth());
+                
+                Node sonLeft = new ExternalNode(newInternalNode, newInternalNode.getDepth() + 1);
+                Block blockSonLeft = new Block(getNextFreeAddress(), factor, templateRecord.getData());
+                ((ExternalNode) sonLeft).setAddressBlock(blockSonLeft.getAddress());
+                
+                Node sonRight = new ExternalNode(newInternalNode, newInternalNode.getDepth() + 1);
+                Block blockSonRight = new Block(getNextFreeAddress(), factor, templateRecord.getData());
+                ((ExternalNode) sonRight).setAddressBlock(blockSonRight.getAddress());
+                
+                ((InternalNode)newInternalNode).setLeftNode(sonLeft);
+                ((InternalNode)newInternalNode).setRightNode(sonRight);
+                
+                // presuvam zaznamy. Tu potrebujes zistit hlbku aktualneho vrchola, co vlastne predstavuje frefix z hash retazca. To znamena ze vsetky zaznamy v tomto
+                // aktualnom vrchole maju rovnaku hodnotu prefixu. Preto potrebuješ pre každy zaznam v bolku zistit dalši bit z has retazca podla kotoreho ich potom 
+                // rozdeliš do synov. Bud do prava(1) alebo do lava(0)
+                
+                
+                System.out.println("Blok je plný");
+                return true;
             }
-        } else { // ak nieje v bloku miesto
-            System.out.println("Blok je plný");
         }
+        // tu budem riešiť koliziu. Daj pozor aby sa vykonal tento kod len pri kolizii. To znamena že pri uspešnom/ neuspešnom vloženi treba mať return z metody.
         return true;
     }
 
